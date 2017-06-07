8524ef7348d78d6ab45e717273d82ae4697098b2
octavarion <octavarion@outlook.com>
2017-06-07 15:55:39 +0200

    Fixed delta kinematics

--- kinematics/delta.py
+++ kinematics/delta.py
@@ -1,6 +1,11 @@
 import math
 import numpy as np
-from . import Kinematics
+if __name__ == '__main__':
+    import time
+    from main import Kinematics
+else:
+    from . import Kinematics
+from scipy.optimize import minimize
 
 
 def unit(v):
@@ -28,15 +33,18 @@ def trilateration(p, r):
 
 
 class DeltaKinematics(Kinematics):
-    def __init__(self, rod_length, radius, endstop_offset):
+    def __init__(self, rod_length, radius, endstop_offset, effector_radius=34.0):
         self.rod_length = rod_length
-        self.radius = radius
+        self.radius = np.array(radius)
+        self.effector_radius = effector_radius
         self.endstop_offset = np.array(endstop_offset)
-        self.towers = [(math.sin(a*math.pi)*r, math.cos(a*math.pi)*r) for a, r in zip(np.linspace(-2/3, 2/3, 3), self.radius)]
+        self.towers = [(math.sin(a*math.pi)*r, math.cos(a*math.pi)*r) for a, r in zip(np.linspace(-2/3, 2/3, 3), self.radius+self.effector_radius)]
 
     def reverse(self, point):
+        px, py, pz = point
         point = np.array(point)
-        distance = [np.linalg.norm(t - point[:-1]) for t in self.towers]
+        points = [np.array((math.sin(a*math.pi)*r+px, math.cos(a*math.pi)*r+py)) for a, r in zip(np.linspace(-2/3, 2/3, 3), [self.effector_radius]*3)]
+        distance = [np.linalg.norm(t - p) for p, t in zip(points, self.towers)]
         try:
             # print(f'rod_length: {self.rod_length} distance: {distance}')
             h = [math.sqrt(r**2 - d**2) + point[-1] for r, d in zip(self.rod_length, distance)]
@@ -49,5 +57,26 @@ class DeltaKinematics(Kinematics):
     def forward(self, x):
         carriage_position = np.array(x) + self.endstop_offset
         centers = np.array([t + (p,) for t, p in zip(self.towers, carriage_position)])
-        result = trilateration(centers.transpose(), np.array(self.rod_length))
-        return result
+        initial = trilateration(centers.transpose(), np.array(self.rod_length))
+
+        def func(params):
+            px, py, pz = params
+            points = [np.array((math.sin(a * math.pi) * r + px, math.cos(a * math.pi) * r + py, pz)) for a, r in
+                      zip(np.linspace(-2 / 3, 2 / 3, 3), [self.effector_radius] * 3)]
+
+            err = np.array([np.linalg.norm(np.subtract(p, c))-r for p, c, r in zip(points, centers, self.rod_length)])
+            return sum(err**2)
+
+        result = minimize(func, initial, method='SLSQP', tol=1e-10)
+        return result.x
+
+if __name__ == '__main__':
+    k1 = DeltaKinematics([190]*3, [84]*3, [0*3])
+    k2 = DeltaKinematics([190] * 3, [84.5] * 3, [0 * 3])
+
+    x = k1.reverse((10.8, 5.0006, 8))
+    t1 = time.perf_counter()
+    val = k2.forward(x)
+    t2 = time.perf_counter()
+    print(t2 - t1)
+    print(val)
